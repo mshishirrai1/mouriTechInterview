f30a248a1d2804b1d267234b15e3c6df25ec824e
prajwalraim <prajwal.m@entoss.tech>
2022-01-30 20:42:35 +0530

    Unwanted imports removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }  from '@angular/common/http'
 
@@ -17,9 +14,6 @@ import { HttpClientModule }  from '@angular/common/http'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatGridListModule,
-    MatFormFieldModule,
-    MatSelectModule,
     FormsModule,
     HttpClientModule
   ],
