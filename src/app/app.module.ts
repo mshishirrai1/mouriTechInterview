afd1ad95676eeb134c1939de56363856532ba35e
prajwalraim <prajwal.m@entoss.tech>
2022-01-30 20:43:20 +0530

    Module formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule }  from '@angular/common/http'
+import { HttpClientModule }  from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 
 
 @NgModule({
